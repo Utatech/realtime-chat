bbf0084cbb6dda62f31be4f33ed40f0d463f1b50
Utatech <prawiraputra64@gmail.com>
2020-09-18 22:12:16 +0700

    combining ver 1 and ver 2

--- application/views/pesan_view.php
+++ application/views/pesan_view.php
@@ -67,7 +67,7 @@
             // FUNCTION SHOW PRODUCT
             function tempatnya(){
                 $.ajax({
-                    url   : '<?php echo site_url("pesan/panggil_nama");?>',
+                    url   : '<?php echo site_url("Pesan/panggil_nama");?>',
                     type  : 'GET',
                     async : true,
                     dataType : 'json',
@@ -89,7 +89,7 @@
                 var nama = $('.name').val();
                 var pesan = $('.price').val();
                 $.ajax({
-                    url    : '<?php echo site_url("pesan/create");?>',
+                    url    : '<?php echo site_url("Pesan/create");?>',
                     method : 'POST',
                     data   : {nama: nama, pesan: pesan},
                     success: function(){
@@ -102,5 +102,12 @@
 
         });
     </script>
+
+    <script>
+    window.__lc = window.__lc || {};
+    window.__lc.license = 12221055;
+    ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[LiveChatWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.livechatinc.com/tracking.js",t.head.appendChild(n)}};!n.__lc.asyncInit&&e.init(),n.LiveChatWidget=n.LiveChatWidget||e}(window,document,[].slice))
+</script>
+<noscript><a href="https://www.livechatinc.com/chat-with/12221055/" rel="nofollow">Chat with us</a>, powered by <a href="https://www.livechatinc.com/?welcome" rel="noopener nofollow" target="_blank">LiveChat</a></noscript>
 </body>
 </html>
